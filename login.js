b0f60ead762b6aa7310043f5df47d62602c32e4a
Berkay Serhan SELVİ <152914048+lihkab@users.noreply.github.com>
2024-10-30 11:57:37 +0300

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("start-game").addEventListener("click", function() {
-    const username = document.getElementById("username").value.trim();
-    
-    if (username) {
-        window.location.href = `game.html?username=${encodeURIComponent(username)}`;
-    } else {
-        alert("Lütfen bir kullanıcı adı girin.");
-    }
-});
